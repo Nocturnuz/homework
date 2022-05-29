30c52461a5abd4859afd191174aa09fedb078ce2
Nocturnuz <106469201+Nocturnuz@users.noreply.github.com>
2022-05-29 12:23:51 +0300

    Add files via upload

---
+++ l2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullname = "Иван Иванович Иванов";
+            int age = 18;
+            string email = "Ivan@mail.ru";
+            float ictmark = 5f;
+            float mathmark = 5f;
+            float physicsmark = 5f;
+            Console.WriteLine("ФИО: " + fullname);
+            Console.WriteLine("Возраст: " + age);
+            Console.WriteLine("email: " + email);
+            Console.WriteLine("Оценка по программированию: " + ictmark);
+            Console.WriteLine("Оценка по математике: " + mathmark);
+            Console.WriteLine("Оецнка по физике: " + physicsmark);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ l2/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Seusing System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float ictmark = 5f;
+            float mathmark = 4.6f;
+            float physicsmark = 4.1f;
+            float marksum = ictmark + mathmark + physicsmark;
+            float mark = marksum / 3;
+            Console.WriteLine("Средняя отметка: " + mark);
+            Console.ReadKey();
+        }
+    }
+}
